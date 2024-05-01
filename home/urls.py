80e53acea02b31587308eaa2dd54c159299018b0
Swayam Patil <patilswayam96@gmail.com>
2024-05-01 08:53:26 +0530

    User Login and registration

--- home/urls.py
+++ home/urls.py
@@ -1,22 +1,24 @@
-"""
-URL configuration for blog_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from . import views
+from .views import UpdatePostView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+#     blogs
+    path("", views.blogs, name="blogs"),
+    path("blog/<str:slug>/", views.blogs_comments, name="blogs_comments"),
+    path("add_blogs/", views.add_blogs, name="add_blogs"),
+    path("edit_blog_post/<str:slug>/", UpdatePostView.as_view(), name="edit_blog_post"),
+    path("delete_blog_post/<str:slug>/", views.Delete_Blog_Post, name="delete_blog_post"),
+    path("search/", views.search, name="search"),
+
+    
+#     profile
+    path("profile/", views.Profile, name="profile"),
+    path("edit_profile/", views.edit_profile, name="edit_profile"),
+    path("user_profile/<int:myid>/", views.user_profile, name="user_profile"),
+    
+#    user authentication
+    path("register/", views.Register, name="register"),
+    path("login/", views.Login, name="login"),
+    path("logout/", views.Logout, name="logout"),
 ]
